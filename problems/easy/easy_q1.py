1598b9b1b2963bff8e683ade75b8d21ed129d571
shridhanya06 <shridhanyasb@outlook.com>
2024-12-09 11:10:11 +0530

    modified easy_q1 with zero error

--- problems/easy/easy_q1.py
+++ problems/easy/easy_q1.py
@@ -10,12 +10,12 @@ if (num % 2) == 0:
  
  
 
+
  
- 
-   print("{0} is Even".format(num))
+   print(f"{0} is Even".format(num))
  
 else:
-   print("{0} is Odd".format(num))
+   print(f"{0} is Odd".format(num))
  
 
 
